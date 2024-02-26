4ef6980765a71c9dc5bd55310973513f283cf327
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-02-26 18:02:23 -0500

    added monte carlo approximations

---
+++ MonteCarlo/montecarlo.py
@@ -0,0 +1,19 @@
+import random 
+import math 
+def main():
+    inside, outside = 0,0
+    # monte carlo approximation of pi, add epochs as needed
+    iterations = 100000
+    for i in range(iterations):
+        x = random.uniform(-0.5,0.5)
+        y = random.uniform(-0.5,0.5)
+        if math.sqrt(x**2 + y **2) <=0.5:
+            inside+=1
+        else:
+            outside+=1
+    expected = math.pi
+    approx = inside/outside
+    error = abs(approx - expected)
+    print(f'Iterations: {iterations}, Value: {inside/outside}, Expected: {expected}, Diff: {error}')
+if __name__ == '__main__':
+    main()
